8d53cca05bdf241c268f4049b3506363690639d0
butayama <butayama@gmail.com>
2020-03-05 23:41:54 +0100

    blauer Hintergrund aber läuft

--- osteotomy/edit_requirements.py
+++ osteotomy/edit_requirements.py
@@ -1,4 +1,4 @@
-# get all modules without version numbers from a requirements_01.txt file
+# get all modules without version numbers from a requirements.txt file
 from os import access, R_OK, W_OK
 from os.path import exists, join, dirname
 from shutil import copy2
@@ -25,14 +25,12 @@ def overwrite_file(filename):
 def strip_version_numbers(f='requirements.txt', f_old='requirements_old.txt'):
     if access(f, R_OK and W_OK):
         try:
-            f_old = overwrite_file(f_old)
+            overwrite_file(f_old)
             copy2(f, f_old)
         except IOError:
             print(f"file {f} could not be copied to {f_old} ")
     else:
-        print(f"file {f} is not accessible for read or write operations.\n")
-        print(" Program aborted")
-        return
+        print(f"file {f} is not accessible for read or write operations.")
     with open(f, 'w') as file_out:
         with open(f_old) as file_object:
             for line in file_object:
