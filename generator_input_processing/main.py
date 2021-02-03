2702426885767ea5cd2da51ea1e71b3c157eadda
butayama <butayama@users.noreply.github.com>
2021-02-03 11:30:57 +0100

    pass test_length

--- generator_input_processing/main.py
+++ generator_input_processing/main.py
@@ -10,6 +10,7 @@ import os
 from sys import version_info
 
 pathname = os.path.join("E:\\", "HiDrive", "generative_art", "files_to_process")
+target_pathname = os.path.join("E:\\", "GitHub", "generator_input_processing", "processed_files")
 
 
 def do_something_with_file_content(csv_path):
@@ -39,31 +40,29 @@ def do_something_with_file_content(csv_path):
     return stone_color_, x_coordinate_, y_coordinate_, z_coordinate_
 
 
-def store_new_file_content(py_filename_, base_name_, stone_color_, x_coordinate_, y_coordinate_, z_coordinate_):
-    csv_path = os.path.join(py_filename_, base_name_ + "new_content_{take}.csv")
+def store_new_file_content(py_filename_, base_name_, stone_color_, x_coordinate_, y_coordinate_, z_coordinate_, take):
+    csv_path = os.path.join(py_filename_, base_name_ + f"new_content_{take}.csv")
     note_header = ["pitch", "duration", "dynamic", "pan", "length"]
     note_lines = [note_header]
 
-    v = []
+    v = [[], [], [], []]
     for i, item in enumerate(z_coordinate_):
-        v[0] = stone_color_[i]
-        v[1] = x_coordinate_[i]
-        v[2] = y_coordinate_[i]
-        v[3] = item
-    v[0].sort()
+        v[0].append(stone_color_[i])
+        v[1].append(x_coordinate_[i])
+        v[2].append(y_coordinate_[i])
+        v[3].append(item)
     v[1].sort()
     v[2].sort()
     v[3].sort()
-
-    for item in v:
-        note_lines.append([str(value) for value in item])
+    for k in range(len(v[3])):
+        note_lines.append([str(v[j][k]) for j in range(len(v))])
 
     if version_info[0] < 3:
         # with open(csv_path, 'wb') as note_file: # python2 OK
         infile = open(csv_path, 'wb')
     else:
         # with open(csv_path, 'w', newline='') as note_file: # python3 OK
-        infile = open(csv_path, 'w', newline='')
+        infile = open(csv_path, 'w+', newline='')
     with infile as note_file:
         note_writer = csv.writer(note_file)
         note_writer.writerows(note_lines)
@@ -79,12 +78,12 @@ def main_loop():
             names = parse_csv
             filename = next(names)
             base_name, _ = os.path.splitext(os.path.basename(filename))
-            py_filename, _ = os.path.splitext(os.path.basename(__file__))
+            py_filename = target_pathname
             stone_color, x_coordinate, y_coordinate, z_coordinate = \
                 do_something_with_file_content(os.path.join(pathname, filename))
-            store_new_file_content(py_filename, base_name, stone_color, x_coordinate,
-                                   y_coordinate, z_coordinate)
             processed_files += 1
+            store_new_file_content(py_filename, base_name, stone_color, x_coordinate,
+                                   y_coordinate, z_coordinate, processed_files)
         except TypeError as e:
             print(e)
             print(f"{py_filename + base_name + '.csv'} could not be processed")
