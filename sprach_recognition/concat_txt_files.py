7f36de33da19be3cf70904385ed2afefd3aca43e
Uwe Schweinsberg <uwe.schweinsberg@t-online.de>
2022-07-28 14:49:58 +0200

    create concat_txt_files.py

--- sprach_recognition/concat_txt_files.py
+++ sprach_recognition/concat_txt_files.py
@@ -2,19 +2,30 @@ import os
 import glob
 
 
-def concat_files(directory):
+def concat_files_binary(directory):
     read_files = glob.glob(directory + '\\*.txt')
     with open("result.txt", "wb") as outfile:
         for f in read_files:
             with open(f, "rb") as infile:
-                outfile.write("====================================================")
-                outfile.write(f)
-                outfile.write("====================================================")
                 outfile.write(infile.read())
+
+
+def concat_files_txt(directory):
+    read_files = glob.glob(directory + '\\*.txt')
+    with open("result.txt", "wb") as outfile:
+        for f in read_files:
+            with open(f, "rb") as infile:
+                outfile.write(f.encode(encoding='utf_8'))
+                outfile.write("\n".encode(encoding='utf_8'))
+                for line in infile:
+                    outfile.write(line)
+                    # outfile.write(f)
+                    # outfile.write("====================================================")
+                    outfile.write("\n".encode(encoding='utf_8'))
 
 
 if __name__ == '__main__':
     txt_directory = 'test_txt_files'
 
     for path in os.listdir(txt_directory):
-        concat_files(txt_directory)
+        concat_files_txt(txt_directory)
